f93158efbeb76aece8e980fb9346d52a46fbbb46
jhj9109 <jhj91_09@naver.com>
2021-12-21 01:21:14 +0900

    Update | 기존 작성 코드들 함수화 |
    get_day_range(soup)
    - 상영 일짜 범위 구하는 함수
    get_screen_data_by_screening
    - 특정 상영의 데이타 구하는 함수
    get_timetable_by_day
    - 특정 일자의 상영의 데이타들을 구하는 함수
    get_whole_timetable
    - 전체 상영 일정에 대한 상영의 데이타들을 구하는 함수

--- main.py
+++ main.py
@@ -38,14 +38,17 @@ def create_url(movie_name, theater_name, theater_type="imax_laser_2d", yyyymmdd=
     if theater_type:
         # 특정영화의 상영시간표
         mcode = movies[movie_name][MOVIE_CODE][theater_type]
-        url = f"{BASE_URL_BY_MOVIE}?midx={midx}&mcode={mcode}&areacode={theaterscode}" # areacode는 13 -> 용산을 의미
+        # areacode는 13 -> 용산을 의미
+        url = f"{BASE_URL_BY_MOVIE}?midx={midx}&mcode={mcode}&areacode={theaterscode}"
     else:
         # 특정상영관의 상영시간표
-        url = f"{BASE_URL_BY_THEATER}?areacode={areacode}&theatercode={theaterscode}&midx={midx}" # areacode는 01 -> 서울을 의미
+        # areacode는 01 -> 서울을 의미
+        url = f"{BASE_URL_BY_THEATER}?areacode={areacode}&theatercode={theaterscode}&midx={midx}"
     if yyyymmdd:
         # 특정일자 기입.
         url += f"&date={yyyymmdd}"
     return url
+
 
 def get_soup_from_url(url):
     response = requests.get(url)  # response 객체
@@ -72,32 +75,46 @@ def get_tags_by_class(soup, target_cls):
     return soup.find_all(is_contain_class_from_tag(target_cls))
 
 
-# 1. 원하는 데이터로 url 생성 후, http 요청 후 soup으로 변환
-request_url_with_out_date = create_url("spider_man", "yongsan")
-html_soup = get_soup_from_url(request_url_with_out_date)
-print(f'요청주소: {request_url_with_out_date}')
+def get_day_range(soup):
+    day_tags = get_tags_by_class(soup, "day")
+    day_range = []
+    for day_tag in day_tags:
+        a_tag = day_tag.a  # 태그 || None
+        yyyymmdd = get_yyyymmdd_by_url(a_tag.get("href"))
+        # span: 월, em: 요일, strong: 일
+        children = a_tag.find_all(True, recursive=False)
+        m = int(children[0].text.strip()[:-1])  # 월: e.g. 12월
+        dd = children[1].text.strip()  # 요일: e.g. 화
+        d = int(children[2].text.strip())  # 일: e.g. 20
+        # print(f"{m}월 {d}일 {dd}요일 yyyymmdd: {yyyymmdd}")
+        day_range.append([m, d, dd, yyyymmdd])
+    print(f"상영 오픈 범위 : {day_range[0][3]} ~ {day_range[-1][3]}")
+    return day_range
 
-# 2. soup에서 원하는 것을 추출하기 위해 파싱
-day_tags = get_tags_by_class(html_soup, "day")
 
-# 2.1 상영 날짜들 추출
-day_range = []
-for day_tag in day_tags:
-    a_tag = day_tag.a  # 태그 || None
-    yyyymmdd = get_yyyymmdd_by_url(a_tag.get("href"))
-    # span: 월, em: 요일, strong: 일
-    children = a_tag.find_all(True, recursive=False)
-    m = int(children[0].text.strip()[:-1])  # 월: e.g. 12월
-    dd = children[1].text.strip()  # 요일: e.g. 화
-    d = int(children[2].text.strip())  # 일: e.g. 20
-    # print(f"{m}월 {d}일 {dd}요일 yyyymmdd: {yyyymmdd}")
-    day_range.append([m, d, dd, yyyymmdd])
-print(f"상영 오픈 범위 : {day_range[0][3]} ~ {day_range[-1][3]}")
-print(day_range)
+def get_screen_data_by_screening(li_tag):
+    a_tag = li_tag.find("a")
+    if (a_tag == None):
+        # 매진 or 상영준비중으로 a태그가 생략 될 수 있다.
+        em_tag = li_tag.find("em")
+        span_tag = li_tag.find("span")
+        screen_data = [em_tag.string, span_tag.string]
+        # print(f"{em_tag.string}시간대 {span_tag.string}")
+    else:
+        # 모든 정보는 a태그의 attr에서 추출 가능하다.
+        theater_name = a_tag.get("data-theatername")
+        screen_kor_name = a_tag.get("data-screenkorname")
+        ymd = a_tag.get("data-playymd")  # data-playymd == yyyymmdd
+        start_time = a_tag.get("data-playstarttime")
+        end_time = a_tag.get("data-playendtime")
+        remain = int(a_tag.get("data-seatremaincnt"))
+        href = a_tag.get("href")
+        screen_data = [theater_name, screen_kor_name, ymd,
+                       start_time, end_time, remain, href]
+    return screen_data
 
-# # 3. 특정 날짜별 HTTP 요청
-i = 0
-for m, d, dd, yyyymmdd in day_range:
+
+def get_timetable_by_day(request_url_with_out_date, yyyymmdd):
     request_url = f"{request_url_with_out_date}&date={yyyymmdd}"
     html_soup = get_soup_from_url(request_url)
     print("\n", "-"*20, f"{yyyymmdd}일자", "-"*20)
@@ -105,24 +122,36 @@ for m, d, dd, yyyymmdd in day_range:
     # 상영관 테이블 -> 해당일자 해당상영관의 상영정보 전체 -> ul
     info_timetable = get_tags_by_class(html_soup, "info-timetable")[0]
     ul_tag = info_timetable.ul
+    
     # 해당일자 해당상영관의 상영정보 하나 -> li
     li_tags = ul_tag.find_all("li", recursive=False)
-
+    screen_data_by_day = []
+    
     for li_tag in li_tags:
-        a_tag = li_tag.find("a")
-        if (a_tag == None):
-            # 매진 or 상영준비중으로 a태그가 생략 될 수 있다.
-            em_tag = li_tag.find("em")
-            span_tag = li_tag.find("span")
-            print(f"{em_tag.string}시간대 {span_tag.string}")
-            continue
-        # 모든 정보는 a태그의 attr에서 추출 가능하다.
-        href = a_tag.get("href")
-        theater_name = a_tag.get("data-theatername")
-        screen_kor_name = a_tag.get("data-screenkorname")
-        ymd = a_tag.get("data-playymd")  # data-playymd == yyyymmdd
-        start_time = a_tag.get("data-playstarttime")
-        end_time = a_tag.get("data-playendtime")
-        remain = int(a_tag.get("data-seatremaincnt"))
-        print(theater_name, screen_kor_name, ymd,
-              start_time, end_time, f"{remain}석", href)
+        screen_data = get_screen_data_by_screening(li_tag)
+        screen_data_by_day.append(screen_data)
+    print(screen_data_by_day)
+    return screen_data_by_day
+
+
+def get_whole_timetable(request_url_with_out_date, day_range):
+    screen_whole_data = dict()
+    for m, d, dd, yyyymmdd in day_range:
+        screen_data_by_day = get_timetable_by_day(request_url_with_out_date, yyyymmdd)
+        screen_whole_data[yyyymmdd] = screen_data_by_day
+    return screen_whole_data
+
+def do():
+    # 1. 원하는 데이터로 url 생성 후, http 요청 후 soup으로 변환
+    request_url_with_out_date = create_url("spider_man", "yongsan")
+    html_soup = get_soup_from_url(request_url_with_out_date)
+    print(f'요청주소: {request_url_with_out_date}')
+
+    # 2. soup에서 원하는 것을 추출하기 위해 파싱 -> 상영 날짜
+    day_range = get_day_range(html_soup)
+
+
+    # # 3. 특정 날짜별 HTTP 요청
+    screen_whole_data = get_whole_timetable(request_url_with_out_date, day_range)
+    
+    return screen_whole_data
