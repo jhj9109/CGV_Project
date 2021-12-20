de563e48dfb7a3c0a105a3af64880e7930bb4aaf
jhj9109 <jhj91_09@naver.com>
2021-12-20 22:34:27 +0900

    Update | Http 요청 url 수정 |
    create_url
    활용하기에 좀 더 편한 주소로 변경

--- main.py
+++ main.py
@@ -33,18 +33,18 @@ def create_url(movie_name, theater_name, theater_type="imax_laser_2d", yyyymmdd=
     areacode = theaters[theater_name][AREA_CODE]
     theaterscode = theaters[theater_name][THEATER_CODE]
     midx = movies[movie_name][MOVIE_ID]
+
     if theater_type:
         # 특정영화의 상영시간표
         mcode = movies[movie_name][MOVIE_CODE][theater_type]
-        url = f"{BASE_URL_BY_MOVIE}?midx={midx}&mcode={mcode}"
+        url = f"{BASE_URL_BY_MOVIE}?midx={midx}&mcode={mcode}&areacode={theaterscode}" # areacode는 13 -> 용산을 의미
     else:
         # 특정상영관의 상영시간표
-        url = f"{BASE_URL_BY_THEATER}?areacode={areacode}&theatercode={theaterscode}&midx={midx}"
+        url = f"{BASE_URL_BY_THEATER}?areacode={areacode}&theatercode={theaterscode}&midx={midx}" # areacode는 01 -> 서울을 의미
     if yyyymmdd:
         # 특정일자 기입.
         url += f"&date={yyyymmdd}"
     return url
-
 
 def get_soup_from_url(url):
     response = requests.get(url)  # response 객체
@@ -66,11 +66,15 @@ def is_contain_class_from_tag(target_cls):
         return is_contain(tag, "class", target_cls)
     return contain_class
 
+
 def get_tags_by_class(soup, target_cls):
     return soup.find_all(is_contain_class_from_tag(target_cls))
+
+
 # 1. 원하는 데이터로 url 생성 후, http 요청 후 soup으로 변환
-request_url = create_url("spider_man", "yongsan")
-html_soup = get_soup_from_url(request_url)
+request_url_with_out_date = create_url("spider_man", "yongsan")
+html_soup = get_soup_from_url(request_url_with_out_date)
+print(f'요청주소: {request_url_with_out_date}')
 
 # 2. soup에서 원하는 것을 추출하기 위해 파싱
 day_tags = get_tags_by_class(html_soup, "day")
@@ -84,6 +88,5 @@ for day_tag in day_tags:
     m = int(children[0].text.strip()[:-1])  # 월: e.g. 12월
     dd = children[1].text.strip()  # 요일: e.g. 화
     d = int(children[2].text.strip())  # 일: e.g. 20
-    # print(f"{month}월 {th}일 {day}요일")
-    day_range.append({"m": m, "d": d, "dd": dd})
+print(f"상영 오픈 범위 : {day_range[0][3]} ~ {day_range[-1][3]}")
 print(day_range)
